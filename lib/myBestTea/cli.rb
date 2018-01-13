2e038d457b1f7745c6c563f8cd6089cf1b2e4bda
Araba Taylor <araba.taylor20@gmail.com>
2018-01-13 21:32:48 +0000

    Mofify cli.rb. Create config.

--- lib/myBestTea/cli.rb
+++ lib/myBestTea/cli.rb
@@ -1,3 +1,24 @@
+# my_best_tea
+#
+# list types of tea
+#
+# What type of tea would you like to learn about?
+#
+# user selects tea type
+#
+# puts information about the tea type
+#
+# Would you like to learn more about this tea?
+#
+# user types y/n
+#
+# If yes...prints information about sub-tea types
+#
+# If no...
+#
+# What type of tea would you like to learn more about?
+
+
 def myBestTea::CLI
 
   def call
@@ -15,7 +36,7 @@ def myBestTea::CLI
   def start
     input = nil
     while input != "n"
-      puts "What type of tea would you like to learn about?"
+      puts "Welcome to My Best Tea! What type of tea would you like to learn about?"
       tea_list
       input = gets.strip
       puts "Would you like to learn more about #{tea name} tea?"
