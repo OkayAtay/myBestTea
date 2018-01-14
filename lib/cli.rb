55487a43eaac0649cc4033738684519f4a2b89e6
Araba Taylor <araba.taylor20@gmail.com>
2018-01-14 17:37:12 +0000

    Wireframe CLI.

--- lib/cli.rb
+++ lib/cli.rb
@@ -22,26 +22,40 @@
 def myBestTea::CLI
 
   def call
+    puts "Welcome to My Best Tea!"
     start
   end
 
   def tea_list
-
+    puts "list of teas"
   end
 
   def print_tea(tea)
+    puts "more information about tea"
+  end
 
+  def tea_types(tea)
+    puts "even more info about specific tea"
   end
 
   def start
     input = nil
-    while input != "n"
-      puts "Welcome to My Best Tea! What type of tea would you like to learn about?"
+    while input != "exit"
       tea_list
+      puts "Please choose a tea to explore. Type exit to leave the program"
       input = gets.strip
-      puts "Would you like to learn more about #{tea name} tea?"
-
+      print_tea(input)
+      puts ""
+      puts "Would you like to learn more about #{input} tea? Type Y or N."
+      puts ""
+      input = gets.strip
+      if second_input == "y"
+        tea_types(second_input)
+      else
+        start
+      end
     end
+     puts "Thanks for using My BestTea -- Happy Sipping!"
   end
 
 end
